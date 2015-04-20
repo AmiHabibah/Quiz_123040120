f9b66f97eba9d904d20c92243c0e18ca16765e07
AmiHabibah <ami.habibah@gmail.com>
2015-04-20 11:58:49 +0700

    Awal

---
+++ src/main/java/Soal/MainSoal.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Soal;
+
+/**
+ *
+ * @author SB 604-16
+ */
+public class MainSoal {
+    public static void main(String[] args) {
+        soal soal=new soal();
+        
+        System.out.println("Perkalian : "+soal.perkalian(5, 5));
+    }
+}
